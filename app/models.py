6d37bbc4b46009c695ce7949c790abd4251d96a6
Mansi khurana <mansikhurana@Mansis-MacBook-Air.local>
2021-05-24 20:37:29 +0530

    changes code structure

---
+++ app/models.py
@@ -0,0 +1,71 @@
+from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
+
+class User(db.Model, UserMixin):
+
+	id = db.Column(db.Integer, primary_key=True)
+
+	username = db.Column(db.String(55))
+
+	email = db.Column(db.String(255))
+
+	password_hash = db.Column(db.String(255))
+
+	def __repr__(self):
+
+		return "{}".format(self.username)
+
+	def check_password(self, password):
+
+		return check_password_hash(self.password_hash, password)
+
+	def set_password(self, password):
+
+		self.password_hash = generate_password_hash(password)
+
+class Pages(db.Model):
+
+	id = db.Column(db.Integer, primary_key=True)
+
+	page_name = db.Column(db.String(25))
+
+	page_url = db.Column(db.String(55))
+
+	is_model = db.Column(db.Integer)
+
+	metaContent = db.relationship('Meta_Content', backref='page')
+
+	def __repr__(self):
+
+		return '{}'.format(self.page_name)
+
+
+
+
+
+
+class Meta_Content(db.Model):
+
+	id = db.Column(db.Integer, primary_key=True)
+
+	page_id = db.Column(db.Integer, db.ForeignKey('pages.id', ondelete='CASCADE'))
+	
+	title = db.Column(db.String(55))
+
+	description = db.Column(db.String(255))
+
+	keywords = db.Column(db.String(255))
+
+	og_type = db.Column(db.String(55))
+
+	og_title = db.Column(db.String(55))
+
+	og_description = db.Column(db.String(255))
+
+	og_image = db.Column(db.String(255))
+
+	canonical = db.Column(db.String(255))
+
+	robots = db.Column(db.String(255))
--- app/routes.py
+++ app/routes.py
@@ -1,45 +1,289 @@
-from app import app
-from app.forms import LoginForm
-from flask_login import current_user, login_required
-from flask import render_template, redirect, url_for
-from app.models import Meta_content, pages_url
-
-@app.route('/login')
-def login():
-
-	form = LoginForm()
-
-	page = Meta_content.query.filter_by(page_id = pages_url.query.filter_by(page_name='Login Page').first().id).first()
-
-	if form.validate_on_submit():
-
-		return redirect(url_for('index'))
-
-	return render_template('login.html', form=form, page=page)
+from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
+from app.models import Pages, Meta_Content, User
+from app.utils import save_picture
+from app import app, login, db
+import os
+from flask_login import current_user, login_user, logout_user, login_required
+from app.form import LoginForm
 
 @login_required
+@app.route('/admin/og-picture', methods=['GET', 'POST'])
+def save_og_image():
+
+	image = request.form['image']
+
+	data  = image.split(';')
+
+	if len(data)>1:
+
+		a = save_picture(image)
+
+		return jsonify({'image_name':a})
+
+	return jsonify({'image_name':None})
+
+
 @app.route('/')
 def index():
 
-	page = Meta_content.query.filter_by(page_id = pages_url.query.filter_by(page_name='Home Page').first().id).first()
-		
-	return render_template('index.html', page=page)
+	page = Pages.query.filter_by(page_name='Home Page').first()
+
+
+	meta_obj = page.metaContent[0]
+
+	if meta_obj.og_image:
+
+
+		image_path = url_for('/static/crop_images', filename = meta_obj.og_image)
+
+	else:
+
+		image_path = None
+
+	return render_template('index.html', page=meta_obj, image_path=image_path)
+
+
+@app.route('/admin/login', methods=['GET', 'POST'])
+def admin_login():
+
+	if current_user.is_authenticated:
+
+		return redirect(url_for('admin'))
+
+	form = LoginForm()
+
+	if form.validate_on_submit():
+
+		user = User.query.filter_by(username=form.username.data).first()
+
+		if user is None or not user.check_password(form.password.data):
+
+			flash('Incorrect Login Details')
+
+			return redirect(url_for('admin_login'))
+
+		login_user(user, remember=form.remember_me.data)
+
+		return redirect(url_for('admin'))
+
+	return render_template('login.html', form=form)
 
 
 
 
-@app.route('/abc.htm')
-def fees_page():
 
-	page = Meta_content.query.filter_by(page_id = pages_url.query.filter_by(page_name='Fees & ISA').first().id).first()
 
-	return render_template('fees.html', page=page)
 
-@app.route('/privacy.htm')
+@app.route('/admin')
+@login_required
+def admin():
+
+	return render_template('admin.html')
+
+
+@app.route('/admin/pages')
+@login_required
+def pages():
+
+
+	page_names = Pages.query.all()
+
+	return render_template('pages.html', page_names=page_names)
+
+
+@app.route('/privacy-policy')
 def privacy_policy():
 
-	page = Meta_content.query.filter_by(page_id = pages_url.query.filter_by(page_name='Privacy Policy').first().id).first()
+	page = Pages.query.filter_by(page_name='Privacy Policy').first()
 
 
-	return render_template('privacy_policy.html', page=page)
+
+
+	meta_obj = page.metaContent[0]
+
+	if meta_obj.og_image:
+
+		image_path = url_for('/static/crop_images', filename = meta_obj.og_image)
+
+	else:
+
+		image_path = None
+
+	return render_template('privacy_policy.html', page=meta_obj, image_path=image_path)
+
+
+@app.route('/fees_ISA')
+def fees_page():
+
+	page = Pages.query.filter_by(page_name='Fees and ISA').first()
+
+	meta_obj = page.metaContent[0]
+
+	if meta_obj.og_image:
+
+		image_path = url_for('/static/crop_images', filename = meta_obj.og_image)
+
+	else:
+
+		image_path = None
+
+	return render_template('privacy_policy.html', page=meta_obj, image_path=image_path)
+
+@app.route('/terms_conditions')
+def terms_conditions():
+
+	page = Pages.query.filter_by(page_name='Terms and Conditions').first()
+
+
+
+	meta_obj = page.metaContent[0]
+
+
+	if meta_obj.og_image:
+
+		image_path = url_for('/static/crop_images', filename = meta_obj.og_image)
+
+	else:
+
+		image_path = None
+
+	return render_template('terms_conditions.html', page=meta_obj, image_path=image_path)
+
+
+@login_required
+@app.route('/admin/pages/edit-request', methods=['GET', 'POST'])
+def pages_edit_request():
+
+	if request.form:
+
+		data = request.form['data']
+
+
+
+	else:
+
+		data = request.args.get('page_name')
+
+
+	page = Pages.query.filter_by(page_name=data).first()
+
+	if page.metaContent:
+
+		meta_obj=page.metaContent[0]
+	else:
+
+		meta_obj = {}
+
+	if meta_obj:
+
+		formData = {'page_name':page.page_name,
+					'page_url':'localhost:5000'+page.page_url,
+					'meta_title':meta_obj.title,
+					'meta_description':meta_obj.description,
+					'meta_keywords':meta_obj.keywords,
+					'meta_robots':meta_obj.robots,
+					'meta_canonical':meta_obj.canonical,
+					'og_type':meta_obj.og_type,
+					'og_title':meta_obj.og_title,
+					'og_description':meta_obj.og_description,
+					'og_image':meta_obj.og_image
+					}
+	else:
+
+		formData = {'page_name':page.page_name,'page_url':page.page_url}
+
+	return jsonify(formData)
+
+@login_required
+@app.route('/flash_messages')
+def flash_messages():
+
+	message = request.args.get('message')
+
+	flash(message)
+
+	return jsonify({'message':'done'})
+
+
+@login_required
+@app.route('/admin/metaContent/edit', methods=['GET', 'POST'])
+def metaContent_edit():
+
+
+	data = request.form
+
+	page = Pages.query.filter_by(page_name=data['page_name']).first()
+
+	
+	if page.metaContent:
+
+		meta_obj = page.metaContent[0]
+		
+		meta_obj.title = data['meta_title']
+
+		meta_obj.description = data['meta_description']
+
+		meta_obj.keywords = data['meta_keywords']
+
+		meta_obj.robots = data['meta_robots']
+
+		meta_obj.canonical = data['canonical']
+
+		meta_obj.og_type = data['og_type']
+
+		meta_obj.og_title = data['og_title']
+
+		meta_obj.og_description = data['og_description']
+
+		if data['image_name']:
+
+			if meta_obj.og_image:
+
+				image_path = os.path.join(app.root_path, 'static/crop_images', meta_obj.og_image)
+
+				os.remove(image_path)
+
+			meta_obj.og_image = data['image_name']
+
+		db.session.add(meta_obj)
+
+		db.session.commit()
+
+		
+
+
+	else:
+
+
+		meta_obj = Meta_Content(title=data['meta_title'], description=data['meta_description'], keywords=data['meta_keywords'], robots=data['meta_robots'], canonical=data['canonical'], og_type=data['og_type'], og_title=data['og_title'], og_description=data['og_description'], page=page)
+
+		db.session.add(meta_obj)
+
+		db.session.commit()
+
+	flash('Your changes have been saved')
+
+
+	return redirect(url_for('pages'))
+
+
+
+@app.context_processor
+def inject_pages():
+
+	base_page_names = Pages.query.all()
+
+	return dict(base_page_names=base_page_names)
+
+
+@login.user_loader
+def load_user(id):
+
+	return User.query.get(int(id))
+
+
+
+
+
+
 
